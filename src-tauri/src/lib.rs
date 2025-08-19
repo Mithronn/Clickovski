2308a8a6b4603e34405b4f17548421a405bb4ddb
Mithronn <denizz_kiziroglu@hotmail.com>
2025-08-19 16:08:09 +0300

    19.08.2025 * Updater issue fixed

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -141,12 +141,13 @@ fn global_shortcut_register(app_handle: tauri::AppHandle) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_zustand::init());
 
     #[cfg(desktop)]
     {
+        builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
         builder = builder.plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
             None, // Some(vec!["--flag1", "--flag2"]),
--- src/app/update/page.tsx
+++ src/app/update/page.tsx
@@ -8,48 +8,76 @@ import { relaunch } from '@tauri-apps/plugin-process';
 import { invoke } from "@tauri-apps/api/core";
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
+import { Progress } from "@/components/ui/progress"
+
 interface IUpdateInfo {
   releaseDate: string | undefined;
   version: string | undefined;
 }
 
+interface IUpdateContent {
+  contentLength: number,
+  downloaded: number;
+}
+
+enum UpdateState {
+  Checking = "Checking",
+  Available = "Available",
+  Downloading = "Downloading",
+  Finished = "Finished"
+}
+
 function Update() {
   const { t } = useTranslation();
-  const [isUpdateState, setUpdateState] = useState("Checking");
+  const [isUpdateState, setUpdateState] = useState(UpdateState.Checking);
   const [isUpdateInfo, setUpdateInfo] = useState<IUpdateInfo>();
+  const [isUpdateContent, setUpdateContent] = useState<IUpdateContent>({ contentLength: 1, downloaded: 0 });
 
   useEffect(() => {
     (async () => {
       // Hide update window on mount to prevent white screen
       invoke("open_updater_on_mount");
       try {
+        setUpdateState(UpdateState.Checking);
         const update = await check();
         if (!update) {
           invoke("global_shortcut_register", { invokeMessage: true });
           return invoke("close_updater_and_open_main");
         }
         if (update) {
+          console.log(update)
           setUpdateInfo({
             releaseDate: update?.date,
             version: update?.version,
           });
 
-          setUpdateState("Downloading");
+          setUpdateState(UpdateState.Downloading);
 
           let downloaded = 0;
           let contentLength = 0;
-          // alternatively we could also call update.download() and update.install() separately
           await update.downloadAndInstall((event) => {
             switch (event.event) {
               case 'Started':
                 contentLength = event.data.contentLength || 0;
+
+                setUpdateContent(state => {
+                  return { ...state, contentLength }
+                });
+
                 console.log(`started downloading ${event.data.contentLength} bytes`);
                 break;
               case 'Progress':
                 downloaded += event.data.chunkLength;
+
+                setUpdateContent(state => {
+                  return { ...state, downloaded }
+                });
+
                 console.log(`downloaded ${downloaded} from ${contentLength}`);
                 break;
               case 'Finished':
+                setUpdateState(UpdateState.Finished);
+
                 console.log('download finished');
                 break;
             }
@@ -83,22 +111,22 @@ function Update() {
 
       <div
         data-tauri-drag-region
-        className="flex flex-col items-center justify-center space-y-3 w-full"
+        className="flex flex-col items-center justify-center space-y-1 w-full"
       >
         <p
           data-tauri-drag-region
           className={`font-Readex font-bold dark:text-white text-black select-none`}
         >
-          {isUpdateState === "Checking"
+          {isUpdateState === UpdateState.Checking
             ? t("checking_for_updates")
-            : isUpdateState === "Available"
+            : isUpdateState === UpdateState.Available
               ? t("updates_downloading")
-              : isUpdateState === "Downloading"
+              : isUpdateState === UpdateState.Downloading
                 ? t("updates_downloading")
                 : t("updates_downloaded")}
         </p>
 
-        {isUpdateState === "Checking" ? (
+        {isUpdateState === UpdateState.Checking ? (
           <div
             data-tauri-drag-region
             className="flex w-full items-center justify-center"
@@ -127,7 +155,7 @@ function Update() {
               ></path>
             </svg>
           </div>
-        ) : isUpdateState === "Available" ? (
+        ) : isUpdateState === UpdateState.Available ? (
           <div
             data-tauri-drag-region
             className="flex w-full flex-col space-y-3 items-center justify-center"
@@ -138,13 +166,13 @@ function Update() {
             >
               <p
                 data-tauri-drag-region
-                className={`font-Readex dark:text-white text-black`}
+                className={`font-Readex dark:text-white text-black/50`}
               >
                 v{isUpdateInfo?.version}
               </p>
               <p
                 data-tauri-drag-region
-                className={`font-Readex dark:text-white text-black`}
+                className={`font-Readex dark:text-white text-black/50`}
               >
                 {isUpdateInfo?.releaseDate
                   ? String(
@@ -169,24 +197,24 @@ function Update() {
               </div>
             </div>
           </div>
-        ) : isUpdateState === "Downloading" ? (
+        ) : isUpdateState === UpdateState.Downloading ? (
           <div
             data-tauri-drag-region
-            className="flex w-full flex-col space-y-3 items-center justify-center"
+            className="flex w-full flex-col space-y-6 items-center justify-center"
           >
             <div
               data-tauri-drag-region
-              className="flex w-full flex-row space-x-4 items-center justify-center"
+              className="flex w-full flex-row space-x-4 items-center justify-center text-sm"
             >
               <p
                 data-tauri-drag-region
-                className={`font-Readex dark:text-white text-black`}
+                className={`font-Readex dark:text-white/50 text-black select-none`}
               >
                 v{isUpdateInfo?.version}
               </p>
               <p
                 data-tauri-drag-region
-                className={`font-Readex dark:text-white text-black`}
+                className={`font-Readex dark:text-white/50 text-black select-none`}
               >
                 {isUpdateInfo?.releaseDate
                   ? String(
@@ -202,43 +230,12 @@ function Update() {
               data-tauri-drag-region
               className="w-full items-center justify-center flex flex-row space-x-6"
             >
-              {/* <div className={`relative w-full rounded-[50px] h-[8px] dark:bg-gray-400 bg-gray-300`}>
-                                            <div
-                                                className="absolute rounded-[50px] w-0 h-[8px] bg-blue-600 duration-150"
-                                                style={{
-                                                    width: `${isUpdateProgress.percent}%`
-                                                }}
-                                            />
-                                        </div> */}
-              <p
-                data-tauri-drag-region
-                className={`font-Readex font-bolddark:text-white text-black`}
-              >
-                {t("downloading")}
-              </p>
-              <svg
-                data-tauri-drag-region
-                className="animate-spin h-5 w-5 text-blue-600"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
+              <div className="w-full px-6">
+                <Progress
                   data-tauri-drag-region
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  data-tauri-drag-region
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+                  value={(Math.min(Math.max(isUpdateContent.downloaded, 0), isUpdateContent.contentLength) / isUpdateContent.contentLength) * 100}
+                />
+              </div>
             </div>
           </div>
         ) : (
@@ -307,7 +304,7 @@ function Update() {
           </div>
         )}
       </div>
-    </div>
+    </div >
   );
 }
 
